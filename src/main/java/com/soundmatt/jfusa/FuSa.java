2837d3653a7f986a9c3158c1cfe4846eabb4d3e5
Matt Jones <47545907+SoundMatt@users.noreply.github.com>
2026-07-28 21:45:15 -0700

    chore: bump to v0.6.2
    Patch release tracking x-FuSa spec v1.15.2 (PRs #49, #50).
    
    Signed-off-by: Matt Jones <47545907+SoundMatt@users.noreply.github.com>

--- src/main/java/com/soundmatt/jfusa/FuSa.java
+++ src/main/java/com/soundmatt/jfusa/FuSa.java
@@ -15,7 +15,7 @@ import java.util.HexFormat;
 public final class FuSa {
 
     /** Current release of java-FuSa. */
-    public static final String VERSION = "0.6.1";
+    public static final String VERSION = "0.6.2";
 
     /** x-FuSa spec version this release implements. */
     public static final String SPEC_VERSION = "1.15.2";
